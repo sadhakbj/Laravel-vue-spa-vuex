b6eb83c3f1dd461d9a948986c3b343b3ecc9acc1
sadhakbj <sadhakbj@gmail.com>
2018-10-07 15:49:09 +0545

    AAP-5 Implement Vue toaster package - [x] installed vue toaster package.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,13 @@ import Router from "./routes";
 import MainApp from "./MainApp";
 import stores from "./stores/index";
 
+/**
+ * Initialize v-toaster
+ */
+import Toaster from "v-toaster";
+import "v-toaster/dist/v-toaster.css";
+Vue.use(Toaster, { timeout: 5000 });
+
 const app = new Vue({
     el: "#app",
 
